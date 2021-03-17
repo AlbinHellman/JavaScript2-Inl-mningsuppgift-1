b6100bd54aa8ef0c4e31b3b39139a99b6f42e157
AlbinHellman <steelalbin@gmail.com>
2021-03-17 02:40:45 +0100

    JavaScript2-Inlämningsuppgift-1

---
+++ JavaScript-2-Inlämningsuppgift-1/uppgift1/src/components/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+<nav>
+    <router-link to="/" class="eCommerce">Min eCommerce</router-link>
+    <div>
+        <router-link class="link" to="/" exact>Startsida</router-link>
+        <router-link class="link" to="/om" exact>Om</router-link>
+        <router-link class="link" to="/products" exact>Produkter</router-link>
+    </div>
+</nav>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    
+    background: rgb(89, 89, 158)
+    
+}
+
+.eCommerce {
+text-decoration: none;
+color: black;
+font-size: 3rem;
+font-weight: 600;
+font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+.link {
+    color: black;
+    text-decoration: none;
+    font-size: 1.3rem;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    display: inline-block;
+    position: relative;
+    padding: 1.5rem;
+}
+
+.link:hover {
+    color: white;
+}
+
+.link.router-link-active::after {
+    content: '';
+    position: absolute;
+    border-bottom: 3px solid black;
+    left: 1rem;
+    bottom: 1rem;
+    right: 1rem;
+}
+
+</style>
---
+++ JavaScript-2-Inlämningsuppgift-1/uppgift1/src/views/Products.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="products">
+      <aside>
+          <div class="knappar">
+              <button><i class="fas fa-th-large"></i></button>
+              <button><i class="fas fa-list"></i></button>
+  </div>
+  <form>
+      <div>
+          <input type="text" placeholder="sök...">
+      </div>
+  </form>
+  <div>
+      <a href="#">Filter</a>
+  </div>
+      </aside>
+      <div class="content">
+          <component :is="comp" />
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            comp: 'Grid'
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
